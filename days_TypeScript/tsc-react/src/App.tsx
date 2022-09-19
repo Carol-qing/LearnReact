7939645bd735ed1c7b4d79e40068641d31deba4e
xuxiaoqing <x13924842538@163.com>
2022-09-19 17:47:34 +0800

    Typescript commit

---
+++ days_TypeScript/tsc-react/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyInput from './components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyInput/>
+    </div>
+  );
+}
+
+export default App;
---
+++ days_TypeScript/tsc-react/src/components/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MyInput = () => {
+  const [value, setValue] = React.useState('')
+
+  // 事件类型是“ChangeEvent”
+  // 我们将 “HTMLInputElement” 传递给 input
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value)
+    console.log(e.target.value);
+    
+  }
+
+  return <input value={value} onChange={onChange} id="input-example"/>
+}
+
+export default MyInput
